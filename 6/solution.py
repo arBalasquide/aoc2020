4454f8561993e4822960da84da43494eaaf76707
arBalasquide <adrian.balasquide@gmail.com>
2020-12-06 11:48:43 -0400

    Code golf

--- 6/solution.py
+++ 6/solution.py
@@ -1,32 +1,16 @@
 #!/usr/bin/env python3
 
 
-def get_questions_answered(answers):
-    count = 0
-    for answer in answers:
-        s = {a for a in answer}
-        count += len(s)
-    
-    return count
-
-
-def get_intersecting_answers(answers): 
-    count = 0
-    for answer in answers:
-        sets = [set(a) for a in answer]
-        intersection = set.intersection(*sets)
-        count += len(intersection)
-    
-    return count
+def get_sum(sets):
+    print("Part one:", sum(len(set.union(*group)) for group in answers))
+    print("Part two:", sum(len(set.intersection(*group)) for group in answers))
 
 
 if __name__ == "__main__":
     with open('input') as f:
-        lines = f.read().split("\n\n")
-
-        # TODO: Find an alternative to this inneficiency
-        answers = [[l for l in line.split()] for line in lines]
-        a = [line.replace("\n", "") for line in lines]
-
-        print("Part one:", get_questions_answered(a))
-        print("Part two:", get_intersecting_answers(answers))
+        answers = [
+            list(set(yes for yes in person.strip()) for person in group.split())
+            for group in f.read().split('\n\n')
+        ]
+    
+        get_sum(answers)
